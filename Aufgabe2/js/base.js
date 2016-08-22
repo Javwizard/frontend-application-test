56512b6d00e722b251cce1c54f565881418889dc
javwizard <joachimgoerges@web.de>
2016-08-22 14:47:03 +0200

    first basic slider

--- Aufgabe2/js/base.js
+++ Aufgabe2/js/base.js
@@ -1,1 +1,20 @@
-// Logik bitte hier
+window.onload=function(){
+    var index=1
+    var slides= document.querySelectorAll(".part");
+    setInterval(function(){
+        if(index<4){
+            slides[index].style["background-image"]="url(./img/"+backgroundImages[index-1];
+        }
+        slide(index);
+          index++;  }, 3000
+        )
+};
+var backgroundImages=["entwicklung.jpg","marketing.jpg","team.jpg"];
+    function slide(index){
+    var slider= document.querySelector("section.content");
+        console.log(slider);
+    var activeSlide= document.querySelector(".part .active");
+        var offset= (index%4)*(-100);
+        
+    slider.style.left=offset +"vw";
+};
